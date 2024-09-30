c61447527e0d91545b2bfbe060313f2504f31e42
Gabbocnc <cianci.gabriele@hotmail.it>
2024-09-30 15:28:34 +0200

    initial commit

---
+++ biglietto.js
@@ -0,0 +1,32 @@
+/* TRACCIA
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+= L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+*/
+
+
+
+//prima fase : preparazione
+/* variabile price */
+let km_Price = 0.21;
+/* variabile km */
+let km = parseFloat(prompt('Inserisci i km da percorrere'));
+/* variabile price */
+let km_Total_Price = km * km_Price;
+/* variabile età */
+let age = parseFloat(prompt('Inserisci età passeggero'));
+/* variabile sconto */
+let Total_price;
+
+
+//seconda fase: elaborazione dati
+
+console.log(km_Total_Price, age, Total_price);
+
+if (age < 18){
+    Total_price = km_Total_Price - (km_Total_Price*0.2);
+} 
